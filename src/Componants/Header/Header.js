fea0b7b4c25e0a09d592abea49f877b321f94b96
Omkar Ambalal Kodmur <omkarkodmur2031@gmail.com>
2023-11-26 22:09:23 +0530

    Responisve updates

--- src/Componants/Header/Header.js
+++ src/Componants/Header/Header.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import {Link} from "react-router-dom"
-import {ImSearch} from "react-icons/im"
+/** @format */
+
+import React from "react";
+import { Link } from "react-router-dom";
+import { ImSearch } from "react-icons/im";
 
 const Header = () => {
   return (
-    <nav className="header">
-        <img src="./logo.png" alt="Netflix logo" />
-        
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/tvShows">Tv Shows</Link>
-            <Link to="/movies">Movies</Link>
-            <Link to="/recentlyAdded">Recently added</Link>
-            {/* <Link to="/My_List">My List</Link>   */}
-        </div>
-        <ImSearch/>
-    </nav>
-  )
-}
+    <nav className='header'>
+      <img className="lg" src='./logo.png' alt='Netflix logo' />
+      <img className="sm"  src='./logoSmall.png' alt='Netflix logo' />
 
-export default Header
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/tvShows'>Tv Shows</Link>
+        <Link to='/movies'>Movies</Link>
+        <Link to='/recentlyAdded'>Recently added</Link>
+        {/* <Link to="/My_List">My List</Link>   */}
+      </div>
+      <ImSearch />
+    </nav>
+  );
+};
+
+export default Header;
